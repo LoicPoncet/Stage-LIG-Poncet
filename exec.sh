1fb001b52b25889e0f0472ffc1381a76569e4469
LoicPoncet <loicponcet05@gmail.com>
2016-06-29 15:07:38 +0200

    Ajout de commentaires pour le scrit et ajout d'arguments

--- exec.sh
+++ exec.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# script réalisant le produit de matrices et prenant 5 arguments
+# $1 taille de matrice de départ
+# $2 valeur d'incrémentation de notre taille de matrice
+# $3 taille de matrice d'arrivée
+# $4 nombre d'exécution par taille de matrice
+# $5 nom du fichier résultat
+
 for i in `seq $1 $2 $3`;
 do
-	for j in `seq 1 30`;
+	for j in `seq 1 $4`;
 	do
-		./run $i $j Mesures/mesures2.csv
+		./run $i $j Mesures/$5
 	done
 done    
